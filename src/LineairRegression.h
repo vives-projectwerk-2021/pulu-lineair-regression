f7da80aa12e98fa35f9475d4f7a54c6f2d4ab42b
Ben Boydens <ben.boydens@student.vives.be>
2021-10-27 20:19:30 +0200

    rico method + calc mean method

--- src/LineairRegression.h
+++ src/LineairRegression.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <cstdio>
+
 class LineairRegression {
+    private:
+        float mean(int* values, int count);
     public:
-        int get_rico(int* xval, int* yval);
+        float get_rico(int* xval, int* yval, int count);
 };
--- src/LininairRegression.cpp
+++ src/LininairRegression.cpp
@@ -1,5 +1,27 @@
 #include "LineairRegression.h"
 
-int LineairRegression::get_rico(int* xval, int* yval) {
-    return 0;
+float LineairRegression::mean(int* values, int count) {
+    float sum = 0;
+    for (int i = 0; i < count; i++) {
+        sum += values[i];
+    }
+    return sum / 10;
+}
+
+float LineairRegression::get_rico(int* xval, int* yval, int count) {
+    float xgem = mean(xval, count);
+    float ygem = mean(yval, count);
+
+    float upper_sum = 0;
+    float lower_sum = 0;
+    for (int i = 0; i < count; i++) {
+        upper_sum += (xval[i] - xgem) * (yval[i] - ygem);
+        lower_sum += (xval[i] - xgem) * (xval[i] - xgem);
+    }
+
+    float b = upper_sum/lower_sum;
+    float a = ygem - (b * xgem);
+
+    printf("%fX + %f\n", b, a);
+    return b;
 }
